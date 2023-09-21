f2026207a98476c74a54f3ded8c66ba579560e57
dwwatk02 <87448112+dwwatk02@users.noreply.github.com>
2023-09-21 09:49:27 -0400

    Add files via upload

--- change_issue_status.py
+++ change_issue_status.py
@@ -8,8 +8,8 @@ import time
 urllib3.disable_warnings()
 
 #API Key
-keyId="7cf6435c-b4a0-2994-960b-cb4529d63782"
-keySecret="kZlMeTHDz7dRtEzCDDrz2nFs1s3r6ZU3ipFWsMS4ZpOp"
+keyId=""
+keySecret=""
 
 app_id = "4d5b6b9b-ec4c-4f98-b16b-b8390c7fa2d9"
 issue_type = "HTML Comments Sensitive Information Disclosure"
